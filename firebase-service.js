76bd8eaa7a991b96f65ebeb3770fc35b2860e2c3
Yaroslavna Lisitsyna <125512829+RissE-Gloom@users.noreply.github.com>
2025-09-27 15:50:25 +0300

    Update firebase-service.js

--- firebase-service.js
+++ firebase-service.js
@@ -3,7 +3,7 @@ class FirebaseService {
     constructor() {
         this.isInitialized = false;
         this.firebaseConfig = {
-            apiKey: "FIREBASE_API_KEY,
+            apiKey: "FIREBASE_API_KEY",
             authDomain: "FIREBASE_AUTH_DOMAIN",
             projectId: "FIREBASE_PROJECT_ID",
             storageBucket: "FIREBASE_STORAGE_BUCKET",
