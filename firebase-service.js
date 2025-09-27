188e92273ddc4b4c62db76e04bbd007e837c841e
Yaroslavna Lisitsyna <125512829+RissE-Gloom@users.noreply.github.com>
2025-09-27 16:21:46 +0300

    Update firebase-service.js

--- firebase-service.js
+++ firebase-service.js
@@ -3,12 +3,12 @@ class FirebaseService {
     constructor() {
         this.isInitialized = false;
         this.firebaseConfig = {
-            apiKey: "FIREBASE_API_KEY",
-            authDomain: "FIREBASE_AUTH_DOMAIN",
-            projectId: "FIREBASE_PROJECT_ID",
-            storageBucket: "FIREBASE_STORAGE_BUCKET",
-            messagingSenderId: "FIREBASE_MESSAGINGSENDERID",
-            appId: "FIREBASE_APPID"
+            apiKey: "AIzaSyAqnTZXQDuCF3QqxhOhwTRXCulDaLO_iUI",
+  authDomain: "berloga-lisy.firebaseapp.com",
+  projectId: "berloga-lisy",
+  storageBucket: "berloga-lisy.firebasestorage.app",
+  messagingSenderId: "266173768415",
+  appId: "1:266173768415:web:46e245024336974a7c3f6a"
         }; 
         this.init();
     }
